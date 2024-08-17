d8021b237956449266fa3c6aca638559efd7b92c
Senya-P <senyapsent@gmail.com>
2024-08-17 15:39:58 +0200

    caching json request results

--- src/UniProtProvider.Runtime/UniProtProvider.Runtime.fs
+++ src/UniProtProvider.Runtime/UniProtProvider.Runtime.fs
@@ -4,6 +4,7 @@ open System
 open FSharp.Json
 open System.Net.Http
 open System.Net.Http.Headers
+open System.Data.HashFunction
 
 // Put any utilities here
 [<AutoOpen>]
@@ -209,6 +210,7 @@ type IncompleteResult =
     }
 
 module TypeGenerator = 
+    open System.IO
 
     let resultSize = 5
     type Params(keyword : string) =
@@ -221,33 +223,62 @@ module TypeGenerator =
         if headers.Contains("Link") then
             let linkHeader = headers.GetValues("Link") |> Seq.head
             let regex = System.Text.RegularExpressions.Regex("cursor=([^&]+)")
-            let match_ = regex.Match(linkHeader)
-            if match_.Success then Some(match_.Groups.[1].Value)
+            let matchResult = regex.Match(linkHeader)
+            if matchResult.Success then Some(matchResult.Groups.[1].Value)
             else None
         else None
 
 
-    let request (query: string) = async {
+    let request (url: string) = async {
         use client = new HttpClient()
-        let! result = client.GetStringAsync(query) |> Async.AwaitTask
+        let! result = client.GetStringAsync(url) |> Async.AwaitTask
         return result
     }
 
 
     let genTypeById (id: string) =
         let parts = [| "https://rest.uniprot.org/uniprotkb/search?query="; id; "&format=json" |]
-        let query = System.String.Concat(parts)
+        let url = System.String.Concat(parts)
         let config = JsonConfig.create(allowUntyped = true, deserializeOption = DeserializeOption.AllowOmit)
-        let jsonTask = request query
+        let jsonTask = request url
         let json = Async.RunSynchronously jsonTask
         let prot = Json.deserializeEx<Result> config json
         prot.results[0]
 
+    let getHashedValue(url:string) =
+        let cityHash = System.Data.HashFunction.CityHash.CityHashFactory.Instance.Create(System.Data.HashFunction.CityHash.CityHashConfig(HashSizeInBits = 64))
+        let hash = cityHash.ComputeHash(System.Text.Encoding.ASCII.GetBytes(url))
+        hash.AsHexString()
+
+    let getPath (url: string) =
+        let hashedValue = getHashedValue url
+
+        let mutable currentDirectory = System.IO.DirectoryInfo(System.IO.Directory.GetCurrentDirectory())
+        while currentDirectory.Parent <> null && currentDirectory.Name <> "UniProtProvider" do
+            currentDirectory <- currentDirectory.Parent
+
+        let path = System.IO.Path.Combine(currentDirectory.FullName, "tmp")
+        if not (System.IO.Directory.Exists(path)) then
+            System.IO.Directory.CreateDirectory(path) |> ignore
+
+        System.IO.Path.Combine(path, hashedValue)
+
+    let cacheResult (path: string, contents: string) =
+        System.IO.File.WriteAllText(path, contents)
+
+
+    let getCachedResult (path: string) =
+    // check timestemps?
+        if System.IO.File.Exists(path) then
+            System.IO.File.ReadAllText path
+        else ""
+
     let LEFT_PARENTHESIS = "%28"
     let RIGHT_PARENTHESIS = "%29"
     let COLON = "%3A"
+    let config = JsonConfig.create(allowUntyped = true, deserializeOption = DeserializeOption.AllowOmit)
 
-    let buildQuery (param: Params) =
+    let buildUrl (param: Params) =
         let mutable parts : string list = []
         parts <- "https://rest.uniprot.org/uniprotkb/search?format=json&size=" :: parts
         parts <- string(resultSize) :: parts
@@ -260,21 +291,26 @@ module TypeGenerator =
         match param.organism with 
         | null -> ()
         | value ->  parts <- "+AND+" + LEFT_PARENTHESIS + "organism_name" + COLON + value + RIGHT_PARENTHESIS :: parts
-        let query = System.String.Concat(parts |> List.rev |> List.toArray)
-        query
+        System.String.Concat(parts |> List.rev |> List.toArray)
 
-    let genTypesByKeyWord (param: Params) = 
-        let query = buildQuery param
-        let config = JsonConfig.create(allowUntyped = true, deserializeOption = DeserializeOption.AllowOmit)
-        let jsonTask = request query //"https://rest.uniprot.org/uniprotkb/search?format=json&size=5&query=insulin+AND+(organism_name:human)"
-        let json = Async.RunSynchronously jsonTask
-        let prot = Json.deserializeEx<IncompleteResult> config json
-        prot
+
+    let genTypesByKeyWord (param: Params) =
+        let url = buildUrl param
+        let path = getPath url
+        let result = getCachedResult path
+        if result = "" then
+            let jsonTask = request url //"https://rest.uniprot.org/uniprotkb/search?format=json&size=5&query=insulin+AND+(organism_name:human)"
+            let json = Async.RunSynchronously jsonTask
+            cacheResult (path, json)
+
+            Json.deserializeEx<IncompleteResult> config json
+        else
+            Json.deserializeEx<IncompleteResult> config result
 
     let getCursor (param: Params) = async {
         use client = new HttpClient()
-        let query = buildQuery param
-        let! response = client.GetAsync(query) |> Async.AwaitTask
+        let url = buildUrl param
+        let! response = client.GetAsync(url) |> Async.AwaitTask
         return parseLinkHeader response.Headers
     }
 
