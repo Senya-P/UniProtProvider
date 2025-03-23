c4a15c8c5ef7380d7a0744b28a93e5ed0cbb8d03
Senya-P <senyapsent@gmail.com>
2025-03-23 18:34:01 +0100

    style enhancements

--- src/UniProtProvider.DesignTime/UniProtProvider.DesignTime.InnerTypes.fs
+++ src/UniProtProvider.DesignTime/UniProtProvider.DesignTime.InnerTypes.fs
@@ -13,8 +13,8 @@ module internal InnerTypes =
     let private nextNumber() = count <- count + 1; count 
 
     let getProteinProperties (props : array<UniProtKBIncomplete>) () =
-        [
-            for i in props do
+        props
+        |> Array.map (fun i ->
                 // recommended name is not always present
                 let name = 
                     if i.proteinDescription.recommendedName.IsSome then
@@ -27,30 +27,32 @@ module internal InnerTypes =
                         ""
                 let propertyName = name + " (" + i.uniProtkbId + ")"
                 let value = i.uniProtkbId
-                let p =
-                    ProvidedProperty(propertyName = propertyName,
+                ProvidedProperty(propertyName = propertyName,
                     propertyType = typeof<Protein>,
                     getterCode = (fun _ -> <@@ getProteinById value |> Async.RunSynchronously @@>))
-                p
-        ]
+        )
+        |> Array.toList
 
     let getOrganismProperties (props: array<TaxonomyIncomplete>) () =
-        [
-            for i in props do
+        props
+        |> Array.map (fun i ->
                 let name = System.String.Concat(i.scientificName, " (", i.taxonId, ")")
                 let value = i.taxonId
-                let p =
-                    ProvidedProperty(propertyName=name,
+                ProvidedProperty(
+                    propertyName=name,
                     propertyType = typeof<Taxonomy>,
-                    getterCode = (fun _ -> <@@ getOrganismById value |> Async.RunSynchronously @@>))
-                p
-        ]
+                    getterCode = (fun _ -> <@@ getOrganismById value |> Async.RunSynchronously @@>)
+                )
+        )
+        |> Array.toList
+
     /// Recursively generates the next set of results
     let rec getNext (param: Params) (outerType: ProvidedTypeDefinition) () =
-        let next = 
-            ProvidedTypeDefinition("InnerType" + string(nextNumber()),
+        let next = ProvidedTypeDefinition(
+            "InnerType" + string(nextNumber()),
             Some typeof<obj>,
-            hideObjectMethods=true)
+            hideObjectMethods=true
+        )
         next.AddMember(ProvidedConstructor([], fun _ -> <@@ obj() @@>))
 
         match param.entity with
@@ -68,19 +70,24 @@ module internal InnerTypes =
             let nextParam = param.Clone() in nextParam.cursor <- cursor.Value
             next.AddMemberDelayed (getNext nextParam next)
 
-        let p =
-            ProvidedProperty(propertyName="More...",
+        ProvidedProperty(
+            propertyName="More...",
             propertyType = next,
-            getterCode = (fun _ -> <@@ obj() @@>))
-        p
+            getterCode = (fun _ -> <@@ obj() @@>)
+        )
     /// Generates a method to retrieve proteins by organism name
     let getByOrganism (param : Params) (outerType: ProvidedTypeDefinition) () = 
         let byOrganism = ProvidedMethod("ByOrganism", [], typeof<obj>)
-        byOrganism.DefineStaticParameters([ProvidedStaticParameter("Name", typeof<string>)], fun methName args ->
+        byOrganism.DefineStaticParameters(
+            [ProvidedStaticParameter("Name", typeof<string>)], 
+            fun methName args ->
             let name = args.[0] :?> string
             param.organism <- name
 
-            let t = ProvidedTypeDefinition("InnerType" + string(nextNumber()), Some typeof<obj>, true)
+            let t = ProvidedTypeDefinition(
+                "InnerType" + string(nextNumber()),
+                Some typeof<obj>,
+                true)
             let result = getProteinsByKeyWord param |> Async.RunSynchronously
             t.AddMembersDelayed(getProteinProperties result.results)
 
@@ -90,14 +97,22 @@ module internal InnerTypes =
                 t.AddMemberDelayed(getNext nextParam t)
             outerType.AddMember(t)
 
-            let m = ProvidedMethod(methName, [], t, invokeCode = fun _ -> <@@ obj() @@>)
+            let m = ProvidedMethod(
+                methName, 
+                [], 
+                t, 
+                invokeCode = fun _ -> <@@ obj() @@>
+            )
             outerType.AddMember(m)
             m
         )
         byOrganism
     /// Generates a method to find proteins related to the taxonomy entry
     let getFindRelated (param : Params) (outerType: ProvidedTypeDefinition) () =
-        let t = ProvidedTypeDefinition("InnerType" + string(nextNumber()), Some typeof<obj>)
+        let t = ProvidedTypeDefinition(
+            "InnerType" + string(nextNumber()),
+            Some typeof<obj>
+        )
         let result = getProteinsByKeyWord param |> Async.RunSynchronously
         t.AddMember(ProvidedConstructor([], fun _ -> <@@ obj() @@>))
         t.AddMembersDelayed(getProteinProperties result.results)
@@ -108,17 +123,18 @@ module internal InnerTypes =
             t.AddMemberDelayed(getNext nextParam t)
         outerType.AddMember(t)
 
-        let p =
-            ProvidedMethod(methodName="FindRelated",
+        ProvidedMethod(
+            methodName="FindRelated",
             parameters = [],
             returnType = t,
-            invokeCode = (fun _ -> <@@ obj() @@>))
-        p
+            invokeCode = (fun _ -> <@@ obj() @@>)
+        )
 
     let getOrganismResults (props: array<TaxonomyIncomplete>) (outerType: ProvidedTypeDefinition) () =
-        [
-            for i in props do
-                let organismResult =  ProvidedTypeDefinition("InnerType" + string(nextNumber()),
+        props
+        |> Array.map (fun i ->
+                let organismResult =  ProvidedTypeDefinition(
+                    "InnerType" + string(nextNumber()),
                     Some typeof<obj>,
                     hideObjectMethods=true)
                 organismResult.AddMember(ProvidedConstructor([], fun _ -> <@@ obj() @@>))
@@ -126,33 +142,35 @@ module internal InnerTypes =
 
                 let name = System.String.Concat(i.scientificName, " (", i.taxonId, ")")
                 let value = i.taxonId
-                let p =
-                    ProvidedProperty(propertyName=name,
+                let p = ProvidedProperty(
+                    propertyName=name,
                     propertyType = typeof<Taxonomy>,
-                    getterCode = (fun _ -> <@@ getOrganismById value |> Async.RunSynchronously @@>))
+                    getterCode = (fun _ -> <@@ getOrganismById value |> Async.RunSynchronously @@>)
+                )
                 organismResult.AddMemberDelayed (fun _-> p)
 
                 let param = Params("")
                 param.taxonId <- string(i.taxonId)
                 organismResult.AddMemberDelayed(getFindRelated param outerType)
 
-                let result = 
-                    ProvidedProperty(propertyName=name,
+                ProvidedProperty(
+                    propertyName=name,
                     propertyType = organismResult,
-                    getterCode = (fun _ -> <@@ obj() @@>))
-                result
-        ]
+                    getterCode = (fun _ -> <@@ obj() @@>)
+                )
+        )
+        |> Array.toList
 
 
     let getSuggestions (sug : array<Suggestion>) (outerType: ProvidedTypeDefinition) () =
-        [
-        for i in sug do
+        sug
+        |> Array.map (fun i ->
             let keyword = i.query.Value.Split [|' '|]
             let name = keyword[0]
             let param = Params(name)
             let result = getProteinsByKeyWord param |> Async.RunSynchronously
-            let suggested = 
-                ProvidedTypeDefinition("InnerType" + string(nextNumber()),
+            let suggested =  ProvidedTypeDefinition(
+                "InnerType" + string(nextNumber()),
                 Some typeof<obj>,
                 hideObjectMethods=true)
 
@@ -166,9 +184,10 @@ module internal InnerTypes =
                 suggested.AddMemberDelayed(getNext nextParam suggested)
 
             outerType.AddMember suggested
-            let p =
-                ProvidedProperty(propertyName = name,
+            ProvidedProperty(
+                propertyName = name,
                 propertyType = suggested,
-                getterCode = (fun _ -> <@@ obj() @@>))
-            p
-        ]
+                getterCode = (fun _ -> <@@ obj() @@>)
+            )
+        )
+        |> Array.toList
--- src/UniProtProvider.DesignTime/UniProtProvider.DesignTime.fs
+++ src/UniProtProvider.DesignTime/UniProtProvider.DesignTime.fs
@@ -10,23 +10,28 @@ open UniProtProvider.DesignTime.InnerTypes
 
 [<TypeProvider>]
 type ById (config : TypeProviderConfig) as this = 
-    inherit TypeProviderForNamespaces (config, assemblyReplacementMap=[("UniProtProvider.DesignTime", "UniProtProvider.Runtime")])
+    inherit TypeProviderForNamespaces (
+        config,
+        assemblyReplacementMap=[("UniProtProvider.DesignTime", "UniProtProvider.Runtime")]
+    )
     let ns = "UniProtProvider"
     let asm = Assembly.GetExecutingAssembly()
     // check we contain a copy of runtime files, and are not referencing the runtime DLL
     do assert (typeof<DataSource>.Assembly.GetName().Name = asm.GetName().Name)  
 
-    let retrieveById = 
-        ProvidedTypeDefinition(
+    let retrieveById = ProvidedTypeDefinition(
         asm, 
         ns, 
         "ById", 
         Some typeof<obj>, 
         hideObjectMethods=true)
     do retrieveById.AddMember(ProvidedConstructor([], fun _ -> <@@ obj() @@>))
-    let parameter = ProvidedStaticParameter("UniProtKBId", typeof<string>)
+    let parameter = ProvidedStaticParameter(
+        "UniProtKBId",
+        typeof<string>
+    )
     do retrieveById.DefineStaticParameters( [parameter], fun typeName args -> 
-        let id = (unbox<string> args.[0])
+        let id = unbox<string> args.[0]
         let result = getProteinById id |> Async.RunSynchronously
         let value = result.primaryAccession
         let name = 
@@ -38,17 +43,20 @@ type ById (config : TypeProviderConfig) as this =
                 result.proteinDescription.alternativeNames.Value[0].fullName.value
             else
                 ""
-        let propertyName =  System.String.Concat(name, " (", result.uniProtkbId, ")")
-        let prot = 
-            ProvidedTypeDefinition(typeName, 
+        let propertyName = name + " (" + result.uniProtkbId + ")"
+        let prot = ProvidedTypeDefinition(
+            typeName,
             Some typeof<obj>,
             hideObjectMethods=true)
+
         prot.AddMember(ProvidedConstructor([], fun _ -> <@@ obj() @@>))
-        let p =
-            ProvidedProperty(propertyName = propertyName,
+
+        let p = ProvidedProperty(
+            propertyName = propertyName,
             propertyType = typeof<Protein>,
             getterCode = (fun _ -> <@@ getProteinById value |> Async.RunSynchronously @@>))
         prot.AddMember p
+
         retrieveById.AddMember prot
         prot
     )
@@ -56,42 +64,51 @@ type ById (config : TypeProviderConfig) as this =
         """<summary>Typed representation of the protein entry of the UniProtKB database.</summary>
             <param name="UniProtKBId">Protein entry accession.</param>
             <returns>Typed representation of the protein entry.</returns>"""
-    do retrieveById.AddXmlDoc(helpText)
+    do retrieveById.AddXmlDoc helpText
     do this.AddNamespace(ns, [retrieveById])
 
 
 [<TypeProvider>]
 type ByTaxonId (config : TypeProviderConfig) as this = 
-    inherit TypeProviderForNamespaces (config, assemblyReplacementMap=[("UniProtProvider.DesignTime", "UniProtProvider.Runtime")])
+    inherit TypeProviderForNamespaces (
+        config,
+        assemblyReplacementMap=[("UniProtProvider.DesignTime", "UniProtProvider.Runtime")]
+    )
     let ns = "UniProtProvider"
     let asm = Assembly.GetExecutingAssembly()
     // check we contain a copy of runtime files, and are not referencing the runtime DLL
     do assert (typeof<DataSource>.Assembly.GetName().Name = asm.GetName().Name)  
 
-    let retrieveById = 
-        ProvidedTypeDefinition(
-        asm, 
-        ns, 
-        "ByTaxonId", 
-        Some typeof<obj>, 
+    let retrieveById = ProvidedTypeDefinition(
+        asm,
+        ns,
+        "ByTaxonId",
+        Some typeof<obj>,
         hideObjectMethods=true)
     do retrieveById.AddMember(ProvidedConstructor([], fun _ -> <@@ obj() @@>))
-    let parameter = ProvidedStaticParameter("TaxonId", typeof<int>)
-    do retrieveById.DefineStaticParameters( [parameter], fun typeName args -> 
-        let id = (unbox<int> args.[0])
+    let parameter = ProvidedStaticParameter(
+        "TaxonId",
+        typeof<int>
+    )
+    do retrieveById.DefineStaticParameters([parameter], fun typeName args ->
+        let id = unbox<int> args.[0]
         let result = getOrganismById id |> Async.RunSynchronously
         let value = result.taxonId
-        let name =  System.String.Concat(result.scientificName, " (", value, ")")
-        let prot = 
-            ProvidedTypeDefinition(typeName, 
+        let name =  result.scientificName + " (" + string value + ")"
+        let prot = ProvidedTypeDefinition(
+            typeName,
             Some typeof<obj>,
             hideObjectMethods=true)
+
         prot.AddMember(ProvidedConstructor([], fun _ -> <@@ obj() @@>))
-        let p =
-            ProvidedProperty(propertyName = name,
+
+        let p = ProvidedProperty(
+            propertyName = name,
             propertyType = typeof<Taxonomy>,
-            getterCode = (fun _ -> <@@ getOrganismById value |> Async.RunSynchronously @@>))
+            getterCode = (fun _ -> <@@ getOrganismById value |> Async.RunSynchronously @@>)
+        )
         prot.AddMember p
+
         retrieveById.AddMember prot
         prot
     )
@@ -99,34 +116,41 @@ type ByTaxonId (config : TypeProviderConfig) as this =
         """<summary>Typed representation of the taxonomy entry of the UniProtKB database.</summary>
             <param name="TaxonId">Unique identifier for the taxonomy entry.</param>
             <returns>Typed representation of the taxonomy entry.</returns>"""
-    do retrieveById.AddXmlDoc(helpText)
+    do retrieveById.AddXmlDoc helpText
     do this.AddNamespace(ns, [retrieveById])
 
 [<TypeProvider>]
 type ByKeyWord (config : TypeProviderConfig) as this = 
-    inherit TypeProviderForNamespaces (config, assemblyReplacementMap=[("UniProtProvider.DesignTime", "UniProtProvider.Runtime")])
+    inherit TypeProviderForNamespaces (
+        config,
+        assemblyReplacementMap=[("UniProtProvider.DesignTime", "UniProtProvider.Runtime")]
+    )
     let ns = "UniProtProvider"
     let asm = Assembly.GetExecutingAssembly()
     do assert (typeof<DataSource>.Assembly.GetName().Name = asm.GetName().Name)
 
-    let retrieveByKeyWord = 
-        ProvidedTypeDefinition(
+    let retrieveByKeyWord = ProvidedTypeDefinition(
         asm,
         ns,
         "ByKeyWord", 
         Some typeof<obj>, 
-        hideObjectMethods=true)
+        hideObjectMethods=true
+    )
     do retrieveByKeyWord.AddMember(ProvidedConstructor([], fun _ -> <@@ obj() @@>))
-    let parameter = ProvidedStaticParameter("KeyWord", typeof<string>)
-    do retrieveByKeyWord.DefineStaticParameters( [parameter], fun typeName args ->
-        let prot = 
-            ProvidedTypeDefinition(typeName, 
-            Some typeof<obj>, 
-            hideObjectMethods=true)
+    let parameter = ProvidedStaticParameter(
+        "KeyWord",
+        typeof<string>
+    )
+    do retrieveByKeyWord.DefineStaticParameters([parameter], fun typeName args ->
+        let prot = ProvidedTypeDefinition(
+            typeName,
+            Some typeof<obj>,
+            hideObjectMethods=true
+        )
         prot.AddMember(ProvidedConstructor([], fun _ -> <@@ obj() @@>))
 
         let keyword = unbox<string> args.[0]
-        let param = Params(keyword)
+        let param = Params keyword
         let result = getProteinsByKeyWord param |> Async.RunSynchronously
 
         if result.results.Length = 0 then
@@ -148,33 +172,39 @@ type ByKeyWord (config : TypeProviderConfig) as this =
         """<summary>Typed representation of the protein entries of the UniProtKB database.</summary>
             <param name="KeyWord">Protein search term.</param>
             <returns>All entries associated with the search term in a paginated list of entries.</returns>"""
-    do retrieveByKeyWord.AddXmlDoc(helpText)
+    do retrieveByKeyWord.AddXmlDoc helpText
     do this.AddNamespace(ns, [retrieveByKeyWord])
 
 [<TypeProvider>]
 type ByOrganism (config : TypeProviderConfig) as this =
-    inherit TypeProviderForNamespaces (config, assemblyReplacementMap=[("UniProtProvider.DesignTime", "UniProtProvider.Runtime")])
+    inherit TypeProviderForNamespaces (
+        config,
+        assemblyReplacementMap=[("UniProtProvider.DesignTime", "UniProtProvider.Runtime")]
+    )
     let ns = "UniProtProvider"
     let asm = Assembly.GetExecutingAssembly()
     do assert (typeof<DataSource>.Assembly.GetName().Name = asm.GetName().Name)
-    let retrieveByOrganism = 
-        ProvidedTypeDefinition(
+    let retrieveByOrganism = ProvidedTypeDefinition(
         asm,
         ns,
-        "ByOrganism", 
-        Some typeof<obj>, 
+        "ByOrganism",
+        Some typeof<obj>,
         hideObjectMethods=true)
     do retrieveByOrganism.AddMember(ProvidedConstructor([], fun _ -> <@@ obj() @@>))
-    let parameter = ProvidedStaticParameter("OrganismName", typeof<string>)
-    do retrieveByOrganism.DefineStaticParameters( [parameter], fun typeName args ->
-        let taxon = 
-            ProvidedTypeDefinition(typeName, 
+    let parameter = ProvidedStaticParameter(
+        "OrganismName",
+        typeof<string>
+    )
+    do retrieveByOrganism.DefineStaticParameters([parameter], fun typeName args ->
+        let taxon = ProvidedTypeDefinition(
+            typeName,
             Some typeof<obj>, 
-            hideObjectMethods=true)
+            hideObjectMethods=true
+        )
         taxon.AddMember(ProvidedConstructor([], fun _ -> <@@ obj() @@>))
 
         let organismName = unbox<string> args.[0]
-        let param = Params(organismName)
+        let param = Params organismName
         let result = getOrganismsByKeyWord param |> Async.RunSynchronously
 
         taxon.AddMembersDelayed(getOrganismResults result.results taxon)
@@ -190,5 +220,5 @@ type ByOrganism (config : TypeProviderConfig) as this =
         """<summary>Typed representation of the taxonomy entries of the UniProtKB database.</summary>
             <param name="OrganismName">Taxonomy search term.</param>
             <returns>All entries associated with the search term in a paginated list of entries.</returns>"""
-    do retrieveByOrganism.AddXmlDoc(helpText)
+    do retrieveByOrganism.AddXmlDoc helpText
     do this.AddNamespace(ns, [retrieveByOrganism])
